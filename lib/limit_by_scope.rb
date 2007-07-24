517f6f16b1c5817fecadf2a330bafdbce9c528f3
acts_as_flinn <acts_as_flinn@1cb8c625-b560-42aa-831f-96e2e91b8fb2>
2007-07-24 03:54:52 +0000

    documentation updates
    git-svn-id: svn://rubyforge.org/var/svn/saas/limit_by_scope/trunk/limit_by_scope@6 1cb8c625-b560-42aa-831f-96e2e91b8fb2

--- lib/limit_by_scope.rb
+++ lib/limit_by_scope.rb
@@ -2,8 +2,10 @@
 # Released under the MIT License.  See the MIT-LICENSE file for more details.
 
 module LimitByScope #:nodoc:
-  class MissingScopeAssociation < RuntimeError; end
-  class MissingLimitColumn < RuntimeError; end
+  class MissingScopeAssociation < RuntimeError #:nodoc:
+  end
+  class MissingLimitColumn < RuntimeError #:nodoc:
+  end
 
   def self.append_features(base)
     super
@@ -14,13 +16,13 @@ module LimitByScope #:nodoc:
     # limited_by(scope = :user, options = {})
     #
     # Options are:
-    # * <tt>:id</tt>: scoped foreign key, defaults to association.foreign_key
-    # * <tt>:class_name</tt>: class name of the current scope class, defaults to association.classify
-    # * <tt>:class_id</tt>: identifier method for current scope class, defaults to primary_key
-    # * <tt>:current</tt>: method to call the current scope object, defaults to :current
-    # * <tt>:column</tt>: column used for quota limit, defaults to "#{scope_association}_limit"
-    # * <tt>:delegate</tt>: association to delegate limit calls to
-    # * <tt>:error</tt>: error message to display when quota has been reached, defaults to 'Quota met #{self.class.limit}' (will interpolate)
+    # * <tt>:id</tt> scoped foreign key, defaults to association.foreign_key
+    # * <tt>:class_name</tt> class name of the current scope class, defaults to association.classify
+    # * <tt>:class_id</tt> identifier method for current scope class, defaults to primary_key
+    # * <tt>:current</tt> method to call the current scope object, defaults to :current
+    # * <tt>:column</tt> column used for quota limit, defaults to "#{scope_association}_limit"
+    # * <tt>:delegate</tt> association to delegate limit calls to
+    # * <tt>:error</tt> error message to display when quota has been reached, defaults to 'Quota met #{self.class.limit}' (will interpolate)
 
     def limit_by_scope(scope_association = :user, options = {})
       scope_id = (options.delete(:id) || scope_association).to_s.foreign_key
